fd82d77bffae4a9ed78a8e865ed61fe535de0b41
Rugut254 <timothyruggut61@gmail.com>
2025-03-12 07:59:39 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@
 num1 =float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-#perform calcultion based on the operation
+#perform calculation based on the operation
 if operation == '+':
    result = num1 + num2
    print(f"{num1} + {num2} = {result}")
@@ -19,7 +19,7 @@ elif operation  == '/':
     result = num1 / num2
     print(f"{num1} / {num2} = {result}")
  else:
-    print("Error! Divisionby zero is not allowed.")   
+    print("Error! Division by zero is not allowed.")   
  else:
 print("Invalid operation.Please enter +,-,*, or /.")
 except ValueError:
